79ede15928ff6791566d12380c177c26a8f86378
igorsans <igor.santos0@hotmail.com>
2022-05-20 17:40:49 -0300

    agora agora agora sim
    acho que agora foi

--- assets/src/controller/controller.js
+++ assets/src/controller/controller.js
@@ -1,9 +1,6 @@
 const usuario = new User
 const viwerPage = new ViwerUser
-console.log(usuario);
-console.log(viwerPage);
 viwerPage.entregaFooter()
 // viwerPage.entregaHeader()
-console.log(usuario.verificaLogado1());
 viwerPage.entregaUser(usuario.verificaLogado1())
-console.log(viwerPage);
+
